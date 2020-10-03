68bb6426eef9f79ba86d45f34c607b4a0b852a7b
LensPlaysGames <lensplaysgames@gmail.com>
2020-10-03 16:52:03 -0700

    Project-Euler_Problems//*Problems 1, 2*/
    Project Euler Problem 1 and Problem 2 all done!!
    
    Woot Woot! hehe

---
+++ Project-Euler_Problems/Problem-1/Project-Euler_Problem-1_CPP/Project-Euler_Problem-1_multiples.cpp
@@ -0,0 +1,49 @@
+/*
+Title: Project-Euler_Problem-1-multiples
+Desc: Lens Solve of Problem One from Project Euler Archives
+
+Project Euler Problem Desc: https://projecteuler.net/problem=1
+    If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+    Find the sum of all the multiples of 3 or 5 below 1000.
+
+CODE BY LENS
+*/
+
+#include <list>
+#include <iostream>
+
+using namespace std;
+
+list<int> multiples;
+
+int find_sum_of_ints(list<int> l) {
+    int count = 0;
+    for (auto i = l.begin(); i != l.end(); i++)
+    {
+        count += *i;
+    }
+    return count;
+}
+
+void print_ints_in_list(list<int> l) {
+    for (auto i = l.begin(); i != l.end(); i++)
+    {
+        cout << *i << "\n";
+    }
+}
+
+int main() {
+    for (int i = 0; i < 1000; i++)
+    {
+        if (i % 3 == 0 || i % 5 == 0) {
+            multiples.push_back(i);
+        }
+    }
+    print_ints_in_list(multiples);
+
+    int sum = find_sum_of_ints(multiples);
+    cout << "\nSum of All Multiples of 3 and 5 is " << sum << "\n";
+
+    return 0;
+}
