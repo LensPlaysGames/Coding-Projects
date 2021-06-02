f69e4e7820d3527740e30e87d51bd9ac09a849ca
LensPlaysGames <lensplaysgames@gmail.com>
2021-06-02 11:12:37 -0700

    It actually works. (De)Serialization into JSON implemented.
    Save button works. Recipes loaded from saveFile on startup. Working on add button next

--- Python/RecipeAppCMD/RecipeAppCMD.py
+++ Python/RecipeAppCMD/RecipeAppCMD.py
@@ -4,8 +4,23 @@
 
 # After the data type is constructed, I need a way to modify it. I'm going to TRY and use PyQt5
 # The plan is to have a list of recipes that are displayed in a 'list' widget in QT, that can each be clicked on to open a dialog box with a table containing that recipes ingredients.
+# I did it! It took way too fucking long, but it worked. I can now see the hard-coded recipes appear in the app, as well as click on them to view their ingredients and ratios. This bit took a good two hours.
+
+# Next would be either the ability to save and load the list of recipes, or the ability to add new recipes and remove them. I think saving and loading is harder, so I'll work on that first.
+# If I can't work it out, I'll move on to just making another window with inputs for a new recipe.
+# So I can't just serialize the 'recipe' class. Meaning I have to create 'transition' methods that turn a recipe into a dictionary or list or something, then another to load from the file and do the reverse.
+# This is awful.
+# 20 minutes later, I've done it, sort of.
+# One recipe can be saved and loaded!
+# Now to extend that to a list of recipes...
+# Done! I can now save a list of recipes. Took me like 10 minutes.
+# Time to work on loading!
 
 import PyQt5.QtWidgets as qtw
+import json
+
+saveFile = "recipes.json"
+recipes = []
 
 class Recipe:
     def __init__(self):
@@ -25,43 +40,36 @@ class Recipe:
     def getIngredients(self):
         return self.ingredients
 
-# DUMMY RECIPE LIST THAT WILL LATER BE SAVED AND LOADED FROM FILE
-recipes = []
-# This is how "IT WORKED!"
-StandardBP = Recipe()
-StandardBP.setName("Black Powder")
-StandardBP.addIngredient("KNO3", 75)
-StandardBP.addIngredient("Charcoal", 15)
-StandardBP.addIngredient("Sulfur", 10)
-print(StandardBP.getIngredients())
-recipes.append(StandardBP)
-print()
-MillerBangor165 = Recipe()
-MillerBangor165.setName("Miller - Bangor (16.5%)")
-MillerBangor165.addIngredient("KNO3", 67)
-MillerBangor165.addIngredient("Al (pyro)", 16.5)
-MillerBangor165.addIngredient("Sulfur", 16.5)
-print(MillerBangor165.getIngredients())
-recipes.append(MillerBangor165)
-print()
-Comp604 = Recipe()
-Comp604.setName("Composition 604")
-Comp604.addIngredient("KNO3", 54)
-Comp604.addIngredient("Al (pyro)", 40)
-Comp604.addIngredient("Sulfur", 5)
-Comp604.addIngredient("Boric Acid", 1)
-print(Comp604.getIngredients())
-recipes.append(Comp604)
-print()
+# Helper function to turn a recipe into serializable data.
+def recipeToTuple(recipe):
+    name = recipe.getName()
+    ingredients = recipe.getIngredients()
+    recipeTuple = (name, ingredients)
+    return recipeTuple
 
-for Recipe in recipes:
-    print(Recipe.getName())
-    print()
-    tmpingredients = Recipe.getIngredients()
-    for key, value in tmpingredients.items():
-        print(key, str(value) + "%")
-    print()
+def saveRecipesJSON():
+    recipeTuples = []
+    for Recipe in recipes:
+        recipeTuples.append(recipeToTuple(Recipe))
+    with open(saveFile, 'w') as f:
+        json.dump(recipeTuples, f, indent=2)
+    print("SAVED RECIPES TO JSON SUCCESSFULLY")
 
+def LoadRecipesFromJSON():
+    # FROM JSON, LOAD LIST OF 'RECIPES', which is really just a tuple with a string (name) and a dictionary (ingredients).
+    with open(saveFile, 'r') as f:
+        recipeTuples = json.load(f)
+
+    for recipeTuple in recipeTuples:
+        # Seperate the tuple for each recipe into a string and a dictionary.
+        name, ingredients = recipeTuple
+        # Use the deserialized recipe information to create a new recipe, and add it to the list.
+        recipe = Recipe()
+        recipe.setName(name)
+        for key, value in ingredients.items():
+            recipe.addIngredient(key, value)
+        recipes.append(recipe)
+    print("LOADED RECIPES FROM JSON SUCCESSFULLY")
 
 class RecipeWindow(qtw.QWidget):
     def __init__(self, recipeName):
@@ -70,13 +78,24 @@ class RecipeWindow(qtw.QWidget):
         self.setWindowTitle(recipeName + " Ingredients") 
         self.setLayout(qtw.QVBoxLayout())
 
-        thisRecipe = Recipe()
-        for Recipe in recipes:
-            if Recipe.getName == recipeName:
-                thisRecipe = Recipe
+        for item in recipes:
+            tmpName = item.getName()
+            if item.getName() == recipeName:
+                currentRecipe = item
 
         ingredientsTable = qtw.QTableWidget()
-        ingredientsTable.setRowCount(len(thisRecipe.getIngredients()))
+        ingredientsTable.setRowCount(len(currentRecipe.getIngredients()))
+        ingredientsTable.setColumnCount(2)
+        ingredientsTable.setHorizontalHeaderItem(0, qtw.QTableWidgetItem("Ingredient"))
+        ingredientsTable.setHorizontalHeaderItem(1, qtw.QTableWidgetItem("Percentage"))
+        
+        i = 0
+        for key, value in currentRecipe.getIngredients().items():
+            ingredientsTable.setItem(i, 0, qtw.QTableWidgetItem(str(key)))
+            ingredientsTable.setItem(i, 1, qtw.QTableWidgetItem(str(value)))
+            i += 1
+
+        self.layout().addWidget(ingredientsTable)
 
         self.show()
 
@@ -115,12 +134,52 @@ class MainWindow(qtw.QWidget):
         list.setSelectionMode(qtw.QAbstractItemView.SingleSelection)
         list.itemClicked.connect(list.clicked)
 
+        saveButton = qtw.QPushButton("Save")
+        saveButton.setToolTip("Saves the current list of recipes")
+        container.layout().addWidget(saveButton, 12, 0, 1, 2)
+        saveButton.clicked.connect(saveRecipesJSON)
+
         for Recipe in recipes:
             qtw.QListWidgetItem(Recipe.getName(), list)
 
         self.layout().addWidget(container)
 
+def main():
+    # INITIALIZE DATA
+    LoadRecipesFromJSON()
 
-app = qtw.QApplication([])
-mw = MainWindow()
-app.exec_()
+    app = qtw.QApplication([])
+    mw = MainWindow()
+    app.exec_()
+
+main()
+
+
+
+
+## This is how "IT WORKED!"
+#StandardBP = Recipe()
+#StandardBP.setName("Black Powder")
+#StandardBP.addIngredient("KNO3", 75)
+#StandardBP.addIngredient("Charcoal", 15)
+#StandardBP.addIngredient("Sulfur", 10)
+#print(StandardBP.getIngredients())
+#recipes.append(StandardBP)
+#print()
+#MillerBangor165 = Recipe()
+#MillerBangor165.setName("Miller - Bangor (16.5%)")
+#MillerBangor165.addIngredient("KNO3", 67)
+#MillerBangor165.addIngredient("Al (pyro)", 16.5)
+#MillerBangor165.addIngredient("Sulfur", 16.5)
+#print(MillerBangor165.getIngredients())
+#recipes.append(MillerBangor165)
+#print()
+#Comp604 = Recipe()
+#Comp604.setName("Composition 604")
+#Comp604.addIngredient("KNO3", 54)
+#Comp604.addIngredient("Al (pyro)", 40)
+#Comp604.addIngredient("Sulfur", 5)
+#Comp604.addIngredient("Boric Acid", 1)
+#print(Comp604.getIngredients())
+#recipes.append(Comp604)
+#print()
